4ebb5ff3f3fba23e8f052585f00d152aabba8890
Robbie Campbell <rcampbell182@gmail.com>
2016-10-02 18:15:22 +0100

    set up request to pull data of landings with mass > 99999

---
+++ public/app.js
@@ -0,0 +1,22 @@
+var requestComplete = function() {
+  console.log( "start of requestComplete function" );
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  var meteoriteData = JSON.parse(jsonString);
+  console.log(meteoriteData);
+}
+
+var makeRequest = function( url, callBack ) {
+  var request = new XMLHttpRequest();
+  request.open( "GET", url );
+  console.log( this );
+  request.onload = callBack;
+  request.send();
+}
+
+var app = function() {
+  var url = "https://data.nasa.gov/resource/y77d-th95.json?$where=mass > 99999";
+  makeRequest( url, requestComplete );
+}
+
+window.onload = app;
